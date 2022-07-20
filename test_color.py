05b5f8c050e7545f6fd331e793614073015718a4
Rishitha0055 <rishitha.k@timingindia.com>
2022-07-20 11:06:12 +0530

    some change

--- test_color.py
+++ test_color.py
@@ -6,6 +6,6 @@ from webcolors import (
 
 
 
-h = input('Enter hex: ').lstrip('#')
-print('RGB =', tuple(int(h[i:i+2], 16) for i in (0, 2, 4)))
+hello = input('Enter hex: ').lstrip('#')
+print('RGB =', tuple(int(hello[i:i+2], 16) for i in (0, 2, 4)))
 
